0ca5ae2eb63938f567e79a23ee822acb6ea14df7
Hannah Gouin <hannah.l.gouin@gmail.com>
2020-05-08 05:39:07 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -19,12 +19,12 @@ class Scraper
   def self.scrape_profile_page(profile_url)
     doc = Nokogiri::HTML(open(profile_url))
     {
-      :twitter => (doc.css(".social_icon_container>a").find{|a| a['href'].include?("twitter.com")} || {})['href'],
-      :linkedin => (doc.css(".social_icon_container>a").find{|a| a['href'].include?("linkedin.com")} || {})['href'],
-      :github => (doc.css(".social_icon_container a").find{|a| a['href'].include?("github.com")} || {})['href'],
-      :blog => (doc.css(".social_icon_container a").find{|a| a['href'].include?("flatironschool.com")} || {})['href'],
+      :twitter => (doc.css(".social-icon-container a").find{|a| a['href'].include?("twitter.com")} || {})['href'],
+      :linkedin => (doc.css(".social-icon-container a").find{|a| a['href'].include?("linkedin.com")} || {})['href'],
+      :github => (doc.css(".social-icon-container a").find{|a| a['href'].include?("github.com")} || {})['href'],
+      :blog => (doc.css(".social-icon-container a").find{|a| a['href'].include?("flatironschool.com")} || {})['href'],
       :profile_quote => doc.css("div.profile_quote").text,
-      :bio => doc.css("div.bio-content content-holder").text
+      :bio => doc.css("div.profile-quote").text
     }
   end
 
